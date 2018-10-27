4b82e64b49d1e6989c7cb3b33fb9b15bc3c19d75
Cathleen Melendez <cathleen@melendez.com>
2018-10-27 11:52:24 -0700

    Took out extra space

--- homework1.js
+++ homework1.js
@@ -80,7 +80,6 @@ var handlers = {};
 handlers.sample = function(data, callback) {
   // Callback a http status code, and a payload object
   callback(406, {'name': 'sample handler'});
-
 };
 
 // Greeting handler
